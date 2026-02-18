f65d24fba5bc98c17820e871e7ad7ff7e3cd621f
Brengus <giorgibregvadze2204@gmail.com>
2026-02-18 14:45:25 +0400

    moving safari video to the first place

--- components/MainHero.tsx
+++ components/MainHero.tsx
@@ -4,7 +4,6 @@ import "../css/mainhero.css";
 import { useSelector } from "react-redux";
 import { useTranslation } from 'next-i18next';
 import { useParams } from "next/navigation";
-// import Image from "next/image";
 import { useState, useEffect } from 'react';
 
 function MainHero() {
@@ -22,10 +21,9 @@ function MainHero() {
             <div id="home" className={`main-hero ${isDarkmode ? "main-hero-dark" : "main-hero-light"}`}>
                 <div className="main-hero-grid">
                     <div className={`main-hero-text ${isDarkmode ? "main-hero-text-dark" : ""} ${lng === "ge" ? "main-hero-text-georgian" : ""}`}>{t("MainHero")}</div>
-                    {/* <Image src="/doggo.webp" width="720" height="381" alt="" className="hero-image" /> */}
-                    <video autoPlay loop muted playsInline poster="fallback.jpg" className="hero-image" >
-                        <source src="output.webm" type="video/webm" />
+                    <video autoPlay loop muted playsInline poster="fallback.jpg" className="hero-image" style={{ filter: 'contrast(1.05) saturate(1.1)' }}>
                         <source src="video.mov" type='video/quicktime; codecs="hvc1"' />
+                        <source src="output.webm" type="video/webm" />
                     </video>
                 </div>
             </div>
