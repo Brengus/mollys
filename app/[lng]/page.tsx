4cbe29bad6ec64d4384f660bf54c31fae95837c5
Brengus <giorgibregvadze2204@gmail.com>
2026-03-20 23:55:45 +0400

    adding pictures in gallery, adding gallery on main page

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -1,5 +1,6 @@
 import MainHero from '@/components/MainHero';
 import Hero from '@/components/Hero';
+import Gallery from "@/components/Gallery";
 import RunningLine from '@/components/RunningLine';
 import Why from '@/components/Why';
 import Map from '@/components/Map';
@@ -23,19 +24,38 @@ export async function generateMetadata({ params }: { params: Promise<{ lng: stri
 async function MainPage({ params }: { params: Promise<{ lng: string }> }) {
     const { lng } = await params;
 
+    const pictures = [
+        { url: "/pics/grooming.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0686.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0688.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0689.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0690.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0691.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0693.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0696.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0753(1).webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_0875.webp", aspect: 3 / 4, loading: "lazy" },
+        { url: "/pics/IMG_1116.webp", aspect: 3 / 4, loading: "lazy" },
+        { url: "/pics/IMG_1423.webp", aspect: 3 / 4, loading: "lazy" },
+        { url: "/pics/IMG_1433.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/IMG_1436.webp", aspect: 16 / 9, loading: "lazy" },
+        { url: "/pics/petshop-molly.webp", aspect: 16 / 9, loading: "lazy" }
+    ];
+
     return (
         <>
             <ClientLanguageSync lng={lng} />
             <section>
                 <MainHero lng={lng} />
             </section>
+            <Why lng={lng} />
             <section style={{ minHeight: 400 }}>
                 <Hero lng={lng} />
             </section>
             <section id="ourpartners" style={{ height: '300px' }}>
                 <RunningLine lng={lng} />
             </section>
-            <Why lng={lng} />
+            <Gallery title="MollysSpace" pictures={pictures} />
             <Blog lng={lng} />
             <section id="map">
                 <div style={{ height: '350px' }}>
--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,48 +1,60 @@
+// 'use client';
+// import Image from "next/image";
+// import { useTranslation } from "next-i18next";
+// import "../css/gallery.css";
+
+// export default function GalleryComponent({ title, pictures }: { title: string, pictures: any[] }) {
+//     const { t } = useTranslation();
+
+//     return (
+//         <>
+//             <h2 className="gallery-title">{t(title).toUpperCase()}</h2>
+//             <div className="gallery">
+//                 {pictures.map((pic) => (
+//                     <div
+//                         key={pic.url}
+//                         className="gallery-item"
+//                     // style={{ aspectRatio: pic.aspect }}
+
+//                     >
+//                         <Image
+//                             src={pic.url}
+//                             alt="Dog photo"
+//                             fill
+//                             sizes="(max-width: 768px) 100vw, 33vw"
+//                             className="gallery-image-fill"
+//                             loading="lazy"
+//                         />
+//                     </div>
+//                 ))}
+//             </div >
+//         </>
+//     );
+// }
+
 'use client';
 import Image from "next/image";
 import { useTranslation } from "next-i18next";
 import "../css/gallery.css";
 
-export default function GalleryComponent() {
+export default function GalleryComponent({ title, pictures }: { title: string, pictures: any[] }) {
     const { t } = useTranslation();
-    const pictures = [
-        { url: "/pics/banner.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/bath.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/coffee.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/daycare.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/daycaretwo.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/daycarethree.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/dog.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/door.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/grooming.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/groomingdog.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/outsidearea.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/reception.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/shampoo.webp", aspect: 16 / 9, loading: "lazy" },
-        { url: "/pics/toys.webp", aspect: 16 / 9, loading: "lazy" }
-    ];
 
     return (
         <>
-            <h2 className="gallery-title">{t("HappyClients").toUpperCase()}</h2>
+            <h2 className="gallery-title">{t(title).toUpperCase()}</h2>
             <div className="gallery">
-                {pictures.map((pic) => (
-                    <div
-                        key={pic.url}
-                        className="gallery-item"
-                        style={{ aspectRatio: pic.aspect }}
-                    >
-                        <Image
+                {pictures.map((pic, index) => (
+                    <div key={pic.url || index} className="gallery-item">
+                        <img
                             src={pic.url}
                             alt="Dog photo"
-                            fill
-                            sizes="(max-width: 768px) 100vw, 33vw"
-                            className="gallery-image-fill"
+                            className="gallery-img-natural"
                             loading="lazy"
                         />
                     </div>
                 ))}
-            </div >
+            </div>
         </>
     );
 }
